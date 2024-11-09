d95083325f64247f5ec59027088d8b73f18b9209
svave123 <95907925+svave123@users.noreply.github.com>
2024-11-09 13:23:48 +0400

    Add files via upload

---
+++ 2 задание - тест калькулятора/calculator.py
@@ -0,0 +1,13 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "На ноль делить нельзя!"
+    return x / y
