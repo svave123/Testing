a43dbd33db40457e43585b69c44a77626e7512df
svave123 <95907925+svave123@users.noreply.github.com>
2024-12-14 14:48:55 +0400

    Add files via upload

--- 4 задание - Тестирование своего сайта/TestingDead-god.cy.js
+++ 4 задание - Тестирование своего сайта/TestingDead-god.cy.js
@@ -1,43 +1,49 @@
 describe('Тестирование функции поиска на сайте dead-god.ru', () => {
+  beforeEach(()=> {
+    cy.visit('https://dead-god.ru/')
+    cy.clearCookies();
+  })
     it('Тест 1: Должен нажать на кнопку поиска и ввести текст в поле ввода', () => {
-
-      cy.visit('https://dead-god.ru/');
-
-      cy.get('.search__input').click();
-
+      cy.get('.search__input').click().should('be.visible');
       cy.get('input[placeholder="Введите запрос"]').type('Poop');
-
       cy.get('[data-unique-id="35"]').click();
-
       cy.get('.info__synergies.info__list-block').click();
-
     });
 
 
     it('Тест 2: Проверка окна "Нашли ошибку?"', () => {
-      cy.visit('https://dead-god.ru/');
-
       cy.contains('button', 'Нашли ошибку?').click({force: true});
-
       cy.get('#issue-location').click();
-
       cy.get('#issue-location').type('Там');
-
       cy.get('textarea.issue__input').click();
-
       cy.get('#issue-description').click();
-
       cy.get('#issue-description[placeholder="Опишите ошибку"]').type('ну крч сам разбирайся');
     });
 
 
-        it('Тест3: Проверка разрешений', () => { 
-          cy.visit('https://dead-god.ru/');
+    it('Тест3: Проверка разрешений', () => { 
+      cy.viewport(1020, 680);
+      cy.viewport('ipad-2');
+    });
 
-          cy.viewport(1020, 680);
+   it('Тест4: Проверка фильтров', () => { 
+    cy.get('.filter__button').click();
+    cy.get('#hide_1').click();
+    cy.get('#pool_golden_chest').click({force: true});
+    cy.get('.filter__button').click();
+    });
 
-          cy.viewport('ipad-2');
-      
-        });
+      it('Тест5: Проверка путей у ссылок ', () => { 
+      cy.contains('Объяснение характеристик').click();
+      cy.location('pathname').should('include', 'stats');
+      cy.go('back');
+      cy.contains('О сайте').click();
+      cy.location('pathname').should('include', 'about');
+      cy.go('back');
+      cy.contains('История изменений').click();
+      cy.location('pathname').should('include', '%d0%b8%d1%81%d1%82%d0%be%d1%80%d0%b8%d1%8f-%d0%b8%d0%b7%d0%bc%d0%b5%d0%bd%d0%b5%d0%bd%d0%b8%d0%b9');
+      cy.go('back');
+       });
       });
-  
+
+
