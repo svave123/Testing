52d3e5d5a1d972a32c6c32e735f4b52b55b49929
svave123 <95907925+svave123@users.noreply.github.com>
2025-02-01 14:25:05 +0400

    Update TestingDead-god.cy.js

--- 4 задание - Тестирование своего сайта/TestingDead-god.cy.js
+++ 4 задание - Тестирование своего сайта/TestingDead-god.cy.js
@@ -1,53 +1,73 @@
 describe("Тестирование функции поиска на сайте dead-god.ru", () => {
-  beforeEach(() => {
-    cy.visit("https://dead-god.ru/");
-    cy.clearCookies();
-  });
-  //     it('Тест 1: Должен нажать на кнопку поиска и ввести текст в поле ввода', () => {
-  //       cy.get('.search__input').click().should('be.visible');
-  //       cy.get('input[placeholder="Введите запрос"]').type('Poop');
-  //       cy.get('[data-unique-id="35"]').click();
-  //       cy.get('.info__synergies.info__list-block').click();
-  //     });
-
-  it('Тест 2: Проверка окна "Нашли ошибку?"', () => {
-    cy.contains("button", "Нашли ошибку?").click({ force: true });
-    cy.get("#issue-location").click();
-    cy.get("#issue-send").click();
-    cy.on("window:alert", (text) => {
-      expect(text).to.equal("Не все поля заполнены");
-      cy.get("#issue-location").type("Там");
-      cy.get("textarea.issue__input").click();
-      cy.get("#issue-desc").click();
-      cy.get('#issue-desc[placeholder="Опишите ошибку"]').type(
-        "ну крч сам разбирайся"
-      );
+    beforeEach(() => {
+      cy.visit("https://dead-god.ru/");
+      cy.clearCookies();
     });
-  });
+        it('Тест 1: Должен нажать на кнопку поиска и ввести текст в поле ввода', () => {
+          cy.get('.search__input').click().should('be.visible');
+          cy.get('input[placeholder="Введите запрос"]').type('Poop');
+          cy.get('[data-unique-id="35"]').click();
+          cy.get('.info__synergies.info__list-block').click();
+        });
+    it('Тест 2: Проверка окна "Нашли ошибку?"', () => {
+      cy.contains("button", "Нашли ошибку?").click({ force: true });
+      cy.get("#issue-location").click();
+      cy.get("#issue-send").click();
+      cy.on("window:alert", (text) => {
+        expect(text).to.equal("Не все поля заполнены");
+        cy.get("#issue-location").type("Там");
+        cy.get("textarea.issue__input").click();
+        cy.get("#issue-desc").click();
+        cy.get('#issue-desc[placeholder="Опишите ошибку"]').type(
+          "ну крч сам разбирайся"
+        );
+      });
+    });
+    it("Тест3: Проверка разрешений", () => {
+      cy.viewport(600, 866);
+      cy.get(".nav-menu__link").should("not.be.visible");
+      cy.get(".nav-button").should("be.visible");
+      cy.get(".nav-button", { multiple: true }).eq(1).click();
+    });
+  
+     it('Тест4: Проверка фильтров', () => {
+      cy.get('.filter__button').click();
+      cy.get('#hide_1').click();
+      cy.get('#pool_golden_chest').click({force: true});
+      cy.get('.filter__button').click();
+      });
 
-  it("Тест3: Проверка разрешений", () => {
-    cy.viewport(600, 866);
-    cy.get(".nav-menu__link").should("not.be.visible");
-    cy.get(".nav-button").should("be.visible");
-    cy.get(".nav-button", { multiple: true }).eq(1).click();
-  });
+    it('Тест5: Проверка путей у ссылок ', () => {
+    cy.contains('Объяснение характеристик').click();
+    cy.location('pathname').should('include', 'stats');
+    cy.go('back');
+    cy.contains('О сайте').click();
+    cy.location('pathname').should('include', 'about');
+    cy.go('back');
+    cy.contains('История изменений').click();
+    cy.location('pathname').should('include', '%d0%b8%d1%81%d1%82%d0%be%d1%80%d0%b8%d1%8f-%d0%b8%d0%b7%d0%bc%d0%b5%d0%bd%d0%b5%d0%bd%d0%b8%d0%b9');
+    cy.go('back');
+        });
+        
+         it('Тест6: Проверка путей у ссылок ', () => {
+            cy.get('.list[data-item-type="item"]').should('be.visible');
+            cy.get('.list[data-item-type="item"]').invoke('hide');
+            cy.get('.list[data-item-type="item"]').should('not.be.visible');
+            });
 
-  //  it('Тест4: Проверка фильтров', () => {
-  //   cy.get('.filter__button').click();
-  //   cy.get('#hide_1').click();
-  //   cy.get('#pool_golden_chest').click({force: true});
-  //   cy.get('.filter__button').click();
-  //   });
+         it('Тест7: Проверка фильтров', () => {
+      cy.get('.filter__button').click();
+      cy.get('label[for="color_7"]').click();
+      cy.get('body').should('have.css', 'background-color', 'rgb(67, 45, 40)');
+      cy.get('label[for="color_2"]').click();
+      cy.get('body').should('have.css', 'background-color', 'rgb(40, 40, 42)');
 
-  //     it('Тест5: Проверка путей у ссылок ', () => {`
-  //     cy.contains('Объяснение характеристик').click();
-  //     cy.location('pathname').should('include', 'stats');
-  //     cy.go('back');
-  //     cy.contains('О сайте').click();
-  //     cy.location('pathname').should('include', 'about');
-  //     cy.go('back');
-  //     cy.contains('История изменений').click();
-  //     cy.location('pathname').should('include', '%d0%b8%d1%81%d1%82%d0%be%d1%80%d0%b8%d1%8f-%d0%b8%d0%b7%d0%bc%d0%b5%d0%bd%d0%b5%d0%bd%d0%b8%d0%b9');
-  //     cy.go('back');
-  //      });
-});
+
+      });
+    it('Тест8: Проверка фильтров', () => {
+        cy.get('.filter__button').click();
+        cy.get('label[for="spacing"]').should('have.css', 'color', 'rgb(204, 102, 102)');
+        cy.get('label[for="spacing"]').click();
+        });
+
+        });
